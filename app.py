4a64c3e7536d78b5a790caf13cbe9a1a7ae8d657
FF <@>
2022-08-22 08:39:51 +0000

    file upload

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import os
+import tempfile
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-from flask import Flask
+UPLOAD_FOLDER = os.path.join(tempfile.gettempdir())
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def hello():
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filename)
+            return "filename=" + filename
+            # return redirect(url_for('download_file', name=filename))
     return app.send_static_file("index.html")
